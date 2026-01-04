9e30ac8c9ff263daa726845a8ded9417babdfbca
Sanjay S <sanjays100111@gmail.com>
2026-01-04 11:50:45 +0530

    DEbug

--- api/cron/index.js
+++ api/cron/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Cron route working ✅");
+router.get("/daily", async (req, res) => {
+  try {
+    const users = await User.find({ isSubscribed: true });
+
+    return res.json({
+      message: "Cron triggered successfully ✅",
+      totalUsers: users.length
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Cron failed" });
+  }
 });
 
 module.exports = router;
