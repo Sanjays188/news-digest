d1c06d9a93818abeb132bfff6d083942ff007b0b
Sanjay S <sanjays100111@gmail.com>
2026-01-04 09:38:23 +0530

    debug

--- api/index.js
+++ api/index.js
@@ -5,20 +5,20 @@ const app = express();
 app.use(express.json());
 
 // connect DB once per serverless container
-let isConnected = false;
-app.use(async (req, res, next) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  next();
-});
+// let isConnected = false;
+// app.use(async (req, res, next) => {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//   }
+//   next();
+// });
 
 // routes
-app.use("/api/auth", require("./auth"));
-app.use("/api/user", require("./user"));
-app.use("/api/digest", require("./digest"));
-app.use("/api/cron", require("./cron"));
+// app.use("/api/auth", require("./auth"));
+// app.use("/api/user", require("./user"));
+// app.use("/api/digest", require("./digest"));
+// app.use("/api/cron", require("./cron"));
 
 // test
 app.get("/", (req, res) => {
